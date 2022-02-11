451b3b2c2fb7d3637045665c03810fe1749e2950
duvancho303 <duvancho303@gmail.com>
2022-02-10 21:33:27 -0700

    adding more code

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const countdown = () => {
-    const date = new Date(' Monday May 30, 2022 00:00:00');
-    console.log(date);
+    const endDate = new Date(' Monday May 30, 2022 00:00:00').getTime()
+    const now = new Date().getTime()
+    
+    const difference = endDate - now
+
+    const seconds = 1000
+    const minutes = seconds * 60
+    const hours = minutes * 60
+    const days = hours * 24
+
+    const timeDays = Math.floor(difference / days)
+    const timeHours = Math.floor((difference % days) / hours)
+    const timeMinutes = Math.floor((difference % hours) / minutes)
+    const timeSeconds = Math.floor((difference % minutes) / seconds)
+
+    console.log(timeSeconds);
 }
 countdown()
